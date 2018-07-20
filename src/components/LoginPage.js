342024ef7d4fba254a965399fa3dbfe1892cb2d8
Ravindra Kr. Kahar <ravindrafk@gmail.com>
2018-07-20 21:36:00 +0530

    Added styles to Login Page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,8 +6,8 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expensify</h1>
-      <p>It's time to get your expenses under control.</p>
-      <button className="button" onClick={startLogin}>Login with Google</button>
+      <p className="box-layout__subtitle">It's time to get your expenses under control.</p>
+      <button className="button__login" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
 );
